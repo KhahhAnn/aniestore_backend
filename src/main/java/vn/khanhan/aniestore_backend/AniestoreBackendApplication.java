c263a8f4aed2f9bed10d1b5c91732cc57cc92b7c
khahhan <khanhanbui1010@gmail.com>
2024-01-09 10:51:15 +0700

    commit

---
+++ src/main/java/vn/khanhan/aniestore_backend/AniestoreBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.khanhan.aniestore_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniestoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniestoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/khanhan/aniestore_backend/AniestoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.khanhan.aniestore_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniestoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
