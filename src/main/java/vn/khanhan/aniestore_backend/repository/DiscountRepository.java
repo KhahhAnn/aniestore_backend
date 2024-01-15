baf5ffb155a2736750e889dbf75c0b0b822e438a
khahhan <khanhanbui1010@gmail.com>
2024-01-15 11:29:19 +0700

    add repository

---
+++ src/main/java/vn/khanhan/aniestore_backend/repository/DiscountRepository.java
@@ -0,0 +1,7 @@
+package vn.khanhan.aniestore_backend.repository;
+
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "discount")
+public interface DiscountRepository {
+}
